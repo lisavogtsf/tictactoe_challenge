8564d90b7174ae6ea2f130679f9ae0b7a0486e67
Lisa Vogt <lisavogt.sf@gmail.com>
2016-05-27 14:53:00 -0700

    gets server running for root route, serving index with embedded javascript templating

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-app.js
+var express = require('express');
+var app = express();
+
+// variables for reuse
+var pageTitle = "";
+
+// set up view engine
+app.set('view engine', 'ejs');
+
+// // allow access to local stylesheet
+// app.use(express.static(__dirname + '/public'));
+
+// set up base route
+app.get('/', function (req, res){
+	// res.send("do you want to play a game?");
+	pageTitle = "TIC TAC TOE";
+	res.render('index', {pageTitle: pageTitle});
+});
+
+
+app.listen((process.env.PORT || 3000), function () {
+	console.log("We only care about port 3000. How does that make the other ports feel?");  
+});
+
+module.exports = app;
