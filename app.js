d2569c4c53da4afc5ec83531ee1fdbfce03dde92
Lisa Vogt <lisavogt.sf@gmail.com>
2016-05-28 08:57:06 -0700

    adds sample route to handle incoming Slack slash command, tidies up some less necessary stuff

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ var pageTitle = "";
 // set up view engine
 app.set('view engine', 'ejs');
 
-// // allow access to local stylesheet
-// app.use(express.static(__dirname + '/public'));
 
 // set up base route
 app.get('/', function (req, res){
@@ -17,9 +15,15 @@ app.get('/', function (req, res){
 	res.render('index', {pageTitle: pageTitle});
 });
 
+app.get('/tack', function (req, res){
+	res.send("do you want to play a game?");
+	pageTitle = "TIC TAC TOE";
+	res.render('index', {pageTitle: pageTitle});
+});
 
+// keeps the app listening
 app.listen((process.env.PORT || 3000), function () {
-	console.log("We only care about port 3000. How does that make the other ports feel?");  
+	console.log("listening at http://localhost:3000");  
 });
 
 module.exports = app;
