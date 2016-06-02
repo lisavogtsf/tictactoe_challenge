88b5f0f81e3e743c3cda1f2ea4b2b17acafe38bc
Lisa Vogt <lisavogt.sf@gmail.com>
2016-06-01 19:11:38 -0700

    adds background image and styles a bit

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
           optimization: 2
         },
         files: {
-          "public/assets/app.css": "public/assets/app.less" // destination file and source file
+          "assets/app.css": "assets/app.less" // destination file and source file
         }
       }
     },
