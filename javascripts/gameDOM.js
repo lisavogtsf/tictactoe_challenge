a6ced6e94ebc7625ee68c8d0ed569ac6d6c4db5b
Lisa Vogt <lisavogt.sf@gmail.com>
2016-06-02 21:24:13 -0700

    turns off pointer events at game over, reorganize a bit

--- javascripts/gameDOM.js
+++ javascripts/gameDOM.js
@@ -28,13 +28,17 @@ document.addEventListener('DOMContentLoaded', function (){
 		if (!makeMove(squareId)) {
 			// error or game over
 			updateMessagesDOM();
-			// need to suspend the game
+			// need to visaully suspend the game
+			gameStage = document.querySelector('#game-stage');
+			gameStage.classList.toggle('game-off');
 		}
 	};
 
 	// listener function to attach to each square 
 	// suring board setup
 	var clickOnSquare = function (e) {
+		var squareClasses;
+		var clickedSquareId;
 		// when elements are clicked on, pass the square information on
 		squareClasses = e.target.classList.value;
 		clickedSquareId = squareClasses[squareClasses.search(/[0-9]/)];
@@ -48,9 +52,8 @@ document.addEventListener('DOMContentLoaded', function (){
 
 		// get board dom elements
 		var square;
-		var squareClasses;
 		var squareId;
-		var clickedSquareId;
+		var gameStage;
 
 		// put event listeners on board DOM elements
 		for (var i = 0; i < GAME_SIZE; i++) {
@@ -58,6 +61,10 @@ document.addEventListener('DOMContentLoaded', function (){
 			square = document.querySelector(squareId);
 			square.addEventListener('click', clickOnSquare);
 		}
+
+		// visually indicate game has started
+		gameStage = document.querySelector('#game-stage');
+		gameStage.classList.toggle('game-off');
 	};
 
 	// stub as we're doing nothing in DOM
@@ -77,6 +84,13 @@ document.addEventListener('DOMContentLoaded', function (){
 
 	// new game starts once DOM content has loaded
 	startNewGameDOM();
+
+	// var playButton = document.querySelector('#play');
+	// playButton.addEventListener()
+	// // clear board
+	// // remove listeners
+	// // remove played class, player 1/2
+	// // add game-off
 
 	
 });
